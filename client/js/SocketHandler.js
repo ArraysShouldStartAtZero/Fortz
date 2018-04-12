913e4a5ef3879797ebc7a9d0f2bb21b14490b131
AndrewGraber <graber15@purdue.edu>
2018-04-12 15:45:10 +0000

    changed server IP

--- client/js/SocketHandler.js
+++ client/js/SocketHandler.js
@@ -1,5 +1,5 @@
 //Make connection
-socket = io.connect('http://localhost:3000');
+socket = io.connect('http://ec2-18-218-106-24.us-east-2.compute.amazonaws.com:3000');
 
 function sock_handler_init() {
   //Send hello message to server
