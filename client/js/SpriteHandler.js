61e8e9d059fbb881ab80a0f4c6872681efc0d51e
Liam Brown <liam@brown-mail.com>
2018-04-11 23:04:16 -0400

    idk

--- client/js/SpriteHandler.js
+++ client/js/SpriteHandler.js
@@ -1,4 +1,4 @@
 function getSprite(sprite){
 	PIXI.loader.add(sprite);
-	this.sprite = new PIXI.Sprite(PIXI.loader.resources[sprite].texture);
+	return new PIXI.Sprite(PIXI.loader.resources[sprite].texture); 
 }
