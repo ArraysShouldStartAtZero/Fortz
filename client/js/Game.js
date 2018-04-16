ca43973ec8ce49c80c4f1aa214c4bb7df1920c0d
Liam Brown <liam@brown-mail.com>
2018-04-16 16:06:08 -0400

    client side spawn player?

--- client/js/Game.js
+++ client/js/Game.js
@@ -1,13 +1,4 @@
-function findSpawn( x , y ){
-	x=10;
-	y=10;
-
-}
-
-
-function spawnPlayer(){
-	val x, y;
-	findSpawn(x,y);
+function spawnPlayer(x,y){
 	val user=new Player(x, y, true, 100);
 	val fort = new Fort(x+window.innerWidth/2,y+window.innerHeight/2, user);		
 
--- client/js/SocketHandler.js
+++ client/js/SocketHandler.js
@@ -8,18 +8,25 @@ function sock_handler_init() {
     socket.emit('hello-client', { username: "Peter Boilermaker" });
   });
 
-  socket.on('fort-placement-server', function{//spawn fort parts and camera start location
-	
+  socket.on('fort-placement-server', function(pos){//spawn fort parts and camera start location
+    //console.log("Server says our position is: (" + pos.posX + ", " + pos.posY + ")");
+	dX=pos.posX;
+	dY=pos.posY;
+	spawnPlayer(dX,dY);
 });
 
-  socket.on('update-server',function{//update object locations to stage from server
-
-});
- socket.on('player-update-server',function{//player-detail specific updates from server
+  socket.on('update-server',function(){//update object locations to stage from server
 
 });
 
-  socket.on('position-server', function(pos) {
-    console.log("Server says our position is: (" + pos.posX + ", " + pos.posY + ")");
-  });
+ socket.on('user-unknown-server',function(){});
+
+ socket.on('player-update-server',function(){//player-detail specific updates from server
+
+});
+
+  socket.on('game-over-server',function(){//display gameover-tryagain for player
+
+});
+
 }
