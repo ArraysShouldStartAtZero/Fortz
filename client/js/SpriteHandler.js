90a6c075fb964eb77e0227d8997806f7407cea7a
Liam Brown <liam@brown-mail.com>
2018-04-16 16:48:19 -0400

    r

--- client/js/SpriteHandler.js
+++ client/js/SpriteHandler.js
@@ -7,10 +7,10 @@ function getSprite(sprite, owner){
 	};
 }
 
-function getSpriteFromSheet(sprite, owner){
+function getSpriteFromSheet(sprite, owner){//so far, just used for resoources ->no magic numbers yet
 	PIXI.loader.add(sprite).load(setUp);
 	function setUp(){
-	PIXI.loader.resources[sprite].texture.frame=new Rectangle((Math.random()*4)*16,(Math.random()*4)*16,16,16);
+	PIXI.loader.resources[sprite].texture.frame=new Rectangle(randomInt(0,3)*16,randomInt(0,3)*16,16,16);
 	var dispObjTemp=new PIXI.Sprite(PIXI.loader.resources[sprite].texture);
 	//window.addEventListener("click",//////// ,false);//Add mouse click listerners/actions to controlHandler
 	 app.stage.addChild(dispObjTemp);
