47ce6e7a0aab52aa65aa771eac0191b51f94c7c5
AndrewGraber <graber15@purdue.edu>
2018-04-18 16:17:47 -0400

    Fixed error in server.js

--- server/server.js
+++ server/server.js
@@ -55,8 +55,8 @@ setInterval(update, 500);
 
 function update() {
   //Send out updates
-  sh.update_server(io);
-  sh.player_update_server(io);
+  sh.update_server(sock);
+  sh.player_update_server(sock);
 
   //Run game logic
   db.getAllUnits(game.update);
