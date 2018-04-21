524dcfa686e79f7d78b7507373edccd13305648f
Liam Brown <omniversesoftworks@gmail.com>
2018-04-21 14:39:48 -0400

    cd

--- client/js/Game.js
+++ client/js/Game.js
@@ -6,7 +6,7 @@ function addGameObject(type, uid, posX, posY, health, owner){
 var adjX=posX*64-cX;//adjusted positions for camera position
 var adjY=posY*64-cY;
 if(type=='WALL'){
-Structure(type,adjX,adjY,health, 'sprites/gry_wall.png' ,owner);
+Structure(type,adjX,adjY,health, 'sprites/gry_twr.png' ,owner);
 }else if(type=='TOWER'){
 Structure(type,adjX,adjY,health, 'sprites/gry_twr.png' ,owner);
 }else if(type=='INFNTR'||type=='CAVLRY'||type=='ARTLRY'){
