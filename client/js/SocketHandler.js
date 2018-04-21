14d47659f3c2f5033f1e3bf5c67ab82ee594d827
Liam Brown <omniversesoftworks@gmail.com>
2018-04-21 16:38:25 -0400

    satge

--- client/js/SocketHandler.js
+++ client/js/SocketHandler.js
@@ -18,7 +18,7 @@ function sock_handler_init() {
 
   socket.on('update-server',function(objects){//update object locations to stage from server
 console.log("Got update!");
-if(app.satge.children.length-1>6){
+if(app.stage.children.length-1>6){
 app.stage.removeChildren(6,app.stage.children.length-1);
 }
 objs=objects;
