d65eeac2dc0f83de335418625717be89ebbb6c60
AndrewGraber <graber15@purdue.edu>
2018-04-23 19:16:17 -0400

    bugfixes

--- server/js/Game.js
+++ server/js/Game.js
@@ -16,7 +16,6 @@ function update(units) {
   }
   db.getAllPlayers(function (players) {
     db.getAllResources(function (resources) {
-      console.log("players: " + players);
       units.forEach((unit, index, array) => {
         if(unit.type === "WORKER") {
           uh.updateWorker(unit, resources, players.get(unit.owner), move);
