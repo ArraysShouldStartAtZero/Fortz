86df252e3a06ec38ebbfbbabf18658d289997d04
Liam Brown <omniversesoftworks@gmail.com>
2018-04-23 21:41:32 -0400

    ncawekuean,sfmbe

--- client/js/SpriteHandler.js
+++ client/js/SpriteHandler.js
@@ -149,6 +149,11 @@ function getSprite(sprite, owner, posX, posY, hp){
 		this.sprite.buttonMode=true;
 this.sprite.x=posX;
 this.sprite.y=posY;
+
+if((player.name!=owner&&"server"!=owner)){//enemy units/structures are red
+	this.sprite.tint=0xff0000;
+}
+
 if((player.name!=owner&&"server"!=owner)||sprite=="sprites/gry_twr.png"||sprite=="sprites/gry_wall.png"){
 	this.sprite.on("click",function(event){//add specific listeners
 	highlight(event.target);
