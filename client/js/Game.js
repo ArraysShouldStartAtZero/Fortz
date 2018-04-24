b892f04a53cf7ec356ebc70b90df4d177bbcfac2
AndrewGraber <graber15@purdue.edu>
2018-04-24 01:28:16 -0400

    updates

--- client/js/Game.js
+++ client/js/Game.js
@@ -2,56 +2,54 @@ var MAX_SIZE=128;
 var objs=[];
 
 function resourceCounter(){
-uiCont.removeChild(counter);
-counter=new PIXI.Text("Resources:"+player.resources);
-uiCont.addChild(counter);
-
+  uiCont.removeChild(counter);
+  counter=new PIXI.Text("Resources:"+player.resources);
+  uiCont.addChild(counter);
 }
 
 function updateChanged(objects){///////
-unitCont.removeChildren();
-//structCont.removeChildren();//use this for now
-var k=0;
-for(var i=0;i<objects.length;i++){//implement a system of removing items, excluding those that stayed in //place
-	if(objects[i].type==='TOWER'||objects[i].type==='WALL'||objects[i].type==='STRGHD'){
-	let rmvble=true;
-	let addable=false;
-	for(var j=0;j<objs.length;j++){///
-	rmvble=true;
-	addable=false;
-		if(objs[j].id===objects[i].id){//
-		rmvble=false;
-		addable=false;
-		if(objs[j].pos_x===objects[i].pos_x&&objs[j].pos_y===objects[i].pos_y){
-			if(objs[j].health!==objects[i].health){
-			var u=1;
-				if(objects[i].type==='STRGHD'){u=9;}
-			structCont.getChildAt(k).mask=healthDisplay(objects[i].health,objects[i].pos_x,objects[i].pos_y,u);
-			}
-		}else{//handle impossible case
-		structCont.removeChildAt(k);
-		k--;
-		addable=true;
-		}
-		break;
+  unitCont.removeChildren();
+  //structCont.removeChildren();//use this for now
+  var k=0;
+  for(var i=0;i<objects.length;i++){//implement a system of removing items, excluding those that stayed in //place
+	  if(objects[i].type==='TOWER'||objects[i].type==='WALL'||objects[i].type==='STRGHD'){
+	    let rmvble=true;
+	    let addable=false;
+	      for(var j=0;j<objs.length;j++){///
+	        rmvble=true;
+	        addable=false;
+		      if(objs[j].id===objects[i].id){//
+		        rmvble=false;
+		        addable=false;
+		        if(objs[j].pos_x===objects[i].pos_x&&objs[j].pos_y===objects[i].pos_y){
+			        if(objs[j].health!==objects[i].health){
+			          var u=1;
+				        if(objects[i].type==='STRGHD'){u=9;}
+			          structCont.getChildAt(k).mask=healthDisplay(objects[i].health,objects[i].pos_x,objects[i].pos_y,u);
+			        }
+		        }else{//handle impossible case
+            structCont.removeChildAt(k);
+		          k--;
+		          addable=true;
+		        }
+		        break;
+		      }else{
+		        rmvble=false;
+          addable=true;
+		      }//
+	      }///
 
-		}else{
-		rmvble=false;
-		addable=true;
-		}//
-	}///
-
-	if(rmvble&&k<structCont.children.length){
-	structCont.removeChildAt(k);
-	k--;
-	}else if(addable){
-addGameObject(objects[i].type,objects[i].id,objects[i].pos_x,objects[i].pos_y,objects[i].health,objects[i].owner);
-	}
-k++;
-	}else{
-addGameObject(objects[i].type,objects[i].id,objects[i].pos_x,objects[i].pos_y,objects[i].health,objects[i].owner);
-}
-}// //
+	      if(rmvble&&k<structCont.children.length){
+	      structCont.removeChildAt(k);
+	      k--;
+	    }else if(addable){
+        addGameObject(objects[i].type,objects[i].id,objects[i].pos_x,objects[i].pos_y,objects[i].health,objects[i].owner);
+	    }
+      k++;
+	  }else{
+      addGameObject(objects[i].type,objects[i].id,objects[i].pos_x,objects[i].pos_y,objects[i].health,objects[i].owner);
+    }
+  }// //
 }///////
 
 function addGameObject(type, uid, posX, posY, health, owner){
